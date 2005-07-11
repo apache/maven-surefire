f6c301f73975254120b9b594a165c68241f40575
Kenney Westerhof <kenney@apache.org>
2005-07-11 22:03:58 +0000

    Applied patch for http://jira.codehaus.org/browse/SUREFIRE-2 with modifications.
    Added 2 testcases to demonstrate increased flexibility.
    
    
    git-svn-id: https://svn.apache.org/repos/asf/maven/surefire/trunk@331345 13f79535-47bb-0310-9956-ffa450edef68

---
+++ surefire/surefire/src/test/org/codehaus/surefire/POJOTest.java
@@ -0,0 +1,18 @@
+package org.codehaus.surefire;
+
+/**
+ * Implementation of a POJO with test methods.
+ */
+public class POJOTest
+{
+    public void testFoo()
+    {
+        System.out.println("Foo");
+    }
+
+    public void testBar()
+    {
+        System.out.println("Bar");
+    }
+
+}
