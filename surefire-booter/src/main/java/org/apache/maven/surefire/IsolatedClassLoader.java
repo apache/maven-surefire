d9256db1bb471757d9271e3fb7810b80703784ca
John Dennis Casey <jdcasey@apache.org>
2005-12-11 00:31:09 +0000

    Rolling back the commented loadClass(..) method.
    git-svn-id: https://svn.apache.org/repos/asf/maven/surefire/trunk@355827 13f79535-47bb-0310-9956-ffa450edef68

--- surefire-booter/src/main/java/org/apache/maven/surefire/IsolatedClassLoader.java
+++ surefire-booter/src/main/java/org/apache/maven/surefire/IsolatedClassLoader.java
@@ -7,7 +7,7 @@ package org.apache.maven.surefire;
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *      http://www.apache.org/licenses/LICENSE-2.0
+ *      http:www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
@@ -50,35 +50,35 @@ public class IsolatedClassLoader
         }
     }
 
-//    public synchronized Class loadClass( String className )
-//        throws ClassNotFoundException
-//    {
-//        Class c = findLoadedClass( className );
-//
-//        ClassNotFoundException ex = null;
-//
-//        if ( c == null )
-//        {
-//            try
-//            {
-//                c = findClass( className );
-//            }
-//            catch ( ClassNotFoundException e )
-//            {
-//                ex = e;
-//
-//                if ( parent != null )
-//                {
-//                    c = parent.loadClass( className );
-//                }
-//            }
-//        }
-//
-//        if ( c == null )
-//        {
-//            throw ex;
-//        }
-//
-//        return c;
-//    }
+    public synchronized Class loadClass( String className )
+        throws ClassNotFoundException
+    {
+        Class c = findLoadedClass( className );
+
+        ClassNotFoundException ex = null;
+
+        if ( c == null )
+        {
+            try
+            {
+                c = findClass( className );
+            }
+            catch ( ClassNotFoundException e )
+            {
+                ex = e;
+
+                if ( parent != null )
+                {
+                    c = parent.loadClass( className );
+                }
+            }
+        }
+
+        if ( c == null )
+        {
+            throw ex;
+        }
+
+        return c;
+    }
 }
