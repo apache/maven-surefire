c5b23d7269508ec83601d1f1c206311bcfdef596
Carlos Sanchez Gonzalez <carlos@apache.org>
2006-01-01 21:55:07 +0000

    XMLReporter writes incorrect encoding in XML-file PR: SUREFIRE-24 Submitted By: Simon Kepp Nielsen
    git-svn-id: https://svn.apache.org/repos/asf/maven/surefire/trunk@360563 13f79535-47bb-0310-9956-ffa450edef68

--- surefire/src/main/java/org/apache/maven/surefire/report/XMLReporter.java
+++ surefire/src/main/java/org/apache/maven/surefire/report/XMLReporter.java
@@ -74,7 +74,7 @@ public class XMLReporter
 
         reportDir.mkdirs();
         
-        writer = new PrintWriter( new FileWriter( reportFile ) );
+        writer = new PrintWriter( reportFile, "UTF-8" );
         
         writer.write("<?xml version=\"1.0\" encoding=\"UTF-8\" ?>\n");
         
