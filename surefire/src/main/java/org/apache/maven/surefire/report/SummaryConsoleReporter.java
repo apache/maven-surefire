95c5e1e4dab11edd34a15e7eb19840fe87724511
Carlos Sanchez Gonzalez <carlos@apache.org>
2006-01-01 21:39:29 +0000

    Set right svn properties
    git-svn-id: https://svn.apache.org/repos/asf/maven/surefire/trunk@360553 13f79535-47bb-0310-9956-ffa450edef68

--- surefire/src/main/java/org/apache/maven/surefire/report/SummaryConsoleReporter.java
+++ surefire/src/main/java/org/apache/maven/surefire/report/SummaryConsoleReporter.java
@@ -23,7 +23,7 @@ import java.io.PrintWriter;
 /**
  * Summary format console reporter.
  * @author <a href="mailto:jruiz@exist.com">Johnny R. Ruiz III</a>
- * @version $Id: SummaryConsoleReporter.java 61 2005-10-07 04:07:33Z jruiz $
+ * @version $Id$
  */
 public class SummaryConsoleReporter
     extends AbstractReporter
