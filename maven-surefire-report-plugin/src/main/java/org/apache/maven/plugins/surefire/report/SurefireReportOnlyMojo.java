963db6acb16779beb1ab676e3741a567c926fcda
Barrie Treloar <baerrach@apache.org>
2007-01-19 01:07:27 +0000

    [MSUREFIREREP-6] Created work-around goal "report-only" until a better solution can be found.
    git-svn-id: https://svn.apache.org/repos/asf/maven/surefire/trunk@497669 13f79535-47bb-0310-9956-ffa450edef68

---
+++ maven-surefire-report-plugin/src/main/java/org/apache/maven/plugins/surefire/report/SurefireReportOnlyMojo.java
@@ -0,0 +1,37 @@
+package org.apache.maven.plugins.surefire.report;
+
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+/**
+ * Creates a nicely formatted Surefire Test Report in html format.
+ * This goal does not run the tests, it only builds the reports.
+ * This is a workaround for 
+ * <a href="http://jira.codehaus.org/browse/MSUREFIREREP-6">http://jira.codehaus.org/browse/MSUREFIREREP-6</a>
+ *
+ * @since 2.3
+ * @author <a href="mailto:baerrach@gmail.com">Barrie Treloar</a>
+ * @goal report-only
+ * @execute phase="validate" lifecycle="surefire" 
+ */
+public class SurefireReportOnlyMojo
+    extends SurefireReportMojo
+{
+   
+}
