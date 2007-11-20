8318c3f0b441291d43d75fa1da63a4b3380a8900
Daniel Fabulich <dfabulich@apache.org>
2007-11-20 23:06:48 +0000

    [SUREFIRE-379] Added integration test; this test doesn't fail yet, though, because the test results are numerically correct; the problem is the missing exception
    git-svn-id: https://svn.apache.org/repos/asf/maven/surefire/trunk@596870 13f79535-47bb-0310-9956-ffa450edef68

---
+++ surefire-integration-tests/src/test/java/org/apache/maven/surefire/its/TestNgBeforeMethodFailure.java
@@ -0,0 +1,30 @@
+package org.apache.maven.surefire.its;
+
+import java.io.File;
+
+import org.apache.maven.integrationtests.AbstractMavenIntegrationTestCase;
+import org.apache.maven.it.Verifier;
+import org.apache.maven.it.util.ResourceExtractor;
+
+/**
+ * Test failures in @BeforeMethod annotation on TestNg suite
+ * 
+ * @author <a href="mailto:dfabulich@apache.org">Dan Fabulich</a>
+ * 
+ */
+public class TestNgBeforeMethodFailure
+    extends AbstractMavenIntegrationTestCase
+{
+    public void testTestNgBeforeMethodFailure ()
+        throws Exception
+    {
+        File testDir = ResourceExtractor.simpleExtractResources( getClass(), "/testng-beforeMethodFailure" );
+
+        Verifier verifier = new Verifier( testDir.getAbsolutePath() );
+        verifier.executeGoal( "test" );
+        verifier.verifyErrorFreeLog();
+        verifier.resetStreams();
+        
+        HelperAssertions.assertTestSuiteResults( 1, 0, 0, 1, testDir );
+    }
+}
---
+++ surefire-integration-tests/src/test/resources/testng-beforeMethodFailure/src/test/java/TestNGSuiteTest.java
@@ -0,0 +1,19 @@
+import org.testng.Assert;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+
+public class TestNGSuiteTest {
+
+    private boolean beforeMethod = false;
+    
+    @BeforeMethod
+    public void beforeMethod() {
+        Assert.fail();
+    }
+	@Test
+	public void testBeforeMethod()
+	{
+		Assert.assertTrue( beforeMethod );
+	}
+}
