597a6c4a8c86184cebbd356b2645fd0273ea4da3
Daniel Fabulich <dfabulich@apache.org>
2007-11-21 02:27:07 +0000

    [SUREFIRE-112] Adding surefire report test.  Right now we just assert that the report got created and that we got an error free log.
    git-svn-id: https://svn.apache.org/repos/asf/maven/surefire/trunk@596907 13f79535-47bb-0310-9956-ffa450edef68

---
+++ surefire-integration-tests/src/test/java/org/apache/maven/surefire/its/TestNgReportTest.java
@@ -0,0 +1,34 @@
+package org.apache.maven.surefire.its;
+
+import java.io.File;
+
+import junit.framework.Assert;
+
+import org.apache.maven.integrationtests.AbstractMavenIntegrationTestCase;
+import org.apache.maven.it.Verifier;
+import org.apache.maven.it.util.ResourceExtractor;
+
+/**
+ * Test surefire-report on TestNG test
+ * 
+ * @author <a href="mailto:dfabulich@apache.org">Dan Fabulich</a>
+ * 
+ */
+public class TestNgReportTest
+    extends AbstractMavenIntegrationTestCase
+{
+    public void testDefaultConfiguration ()
+        throws Exception
+    {
+        File testDir = ResourceExtractor.simpleExtractResources( getClass(), "/testng-simple" );
+
+        Verifier verifier = new Verifier( testDir.getAbsolutePath() );
+        verifier.executeGoal( "surefire-report:report" );
+        verifier.verifyErrorFreeLog();
+        verifier.resetStreams();
+        
+        HelperAssertions.assertTestSuiteResults( 1, 0, 0, 0, testDir );
+        File reportHtml = new File(testDir, "target/site/surefire-report.html");
+        Assert.assertTrue( "surefire-report is missing", reportHtml.exists() );
+    }
+}
