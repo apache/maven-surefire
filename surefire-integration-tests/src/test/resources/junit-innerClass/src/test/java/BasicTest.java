23a1ea5fbe2acd04bf30607037f73d819650ee3b
Daniel Fabulich <dfabulich@apache.org>
2007-11-23 06:30:53 +0000

    [SUREFIRE-44] Integration test for Surefire-44; passes.
    git-svn-id: https://svn.apache.org/repos/asf/maven/surefire/trunk@597570 13f79535-47bb-0310-9956-ffa450edef68

---
+++ surefire-integration-tests/src/test/resources/junit-innerClass/src/test/java/BasicTest.java
@@ -0,0 +1,14 @@
+import junit.framework.TestCase;
+
+public class BasicTest
+    extends TestCase
+{
+
+    public void testFoo() {
+        new Foo("x", "y");
+    }
+    
+    public class Foo {
+        public Foo(String x, String y) {};
+    }
+}
