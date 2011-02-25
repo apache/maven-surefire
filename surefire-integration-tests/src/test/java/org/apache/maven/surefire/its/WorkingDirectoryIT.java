158e1ca887b2ee9d12e0cf330f2f6994642cbe21
Olivier Lamy <olamy@apache.org>
2011-02-25 22:45:11 +0000

    try a more robust symlink detection
    git-svn-id: https://svn.apache.org/repos/asf/maven/surefire/trunk@1074730 13f79535-47bb-0310-9956-ffa450edef68

--- surefire-integration-tests/src/test/java/org/apache/maven/surefire/its/WorkingDirectoryIT.java
+++ surefire-integration-tests/src/test/java/org/apache/maven/surefire/its/WorkingDirectoryIT.java
@@ -79,7 +79,7 @@ public class WorkingDirectoryIT
         assertNotNull( "user.dir was null in property file", userDirPath );
         File userDir = new File( userDirPath );
         // test if not a symlink
-        if ( ! FileUtils.isSymlink( userDir) )
+        if ( userDir.getCanonicalFile().equals(userDir.getAbsoluteFile()) )
         {
             assertEquals( "wrong user.dir ! symlink", childTestDir.getAbsolutePath(), userDir.getAbsolutePath() );
         }
