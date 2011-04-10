07a71154db7678654b9f7371eadf3bc50a5246db
Stefan Birkner <mail@stefan-birkner.de>
2011-04-10 20:25:47 +0200

    [SUREFIRE-715] added integration test

---
+++ surefire-integration-tests/src/test/java/org/apache/maven/surefire/its/WorkingDirectoryIsInvalidPropertyIT.java
@@ -0,0 +1,38 @@
+package org.apache.maven.surefire.its;
+
+import java.io.File;
+import java.io.IOException;
+
+import org.apache.maven.it.VerificationException;
+import org.apache.maven.it.Verifier;
+import org.apache.maven.it.util.ResourceExtractor;
+
+/**
+ * Test when the configured working directory is an invalid property, SUREFIRE-715
+ */
+public class WorkingDirectoryIsInvalidPropertyIT
+    extends AbstractSurefireIntegrationTestClass
+{
+    private File testDir;
+
+    public void setUp()
+        throws IOException
+    {
+        testDir = ResourceExtractor.simpleExtractResources( getClass(), "/working-directory-is-invalid-property" );
+    }
+
+    public void testWorkingDirectory()
+        throws Exception
+    {
+        Verifier verifier = new Verifier( testDir.getAbsolutePath() );
+        try
+        {
+            executeGoal( verifier, "test" );
+        }
+        catch ( VerificationException e )
+        {
+        }
+        verifier.verifyTextInLog( "The property workingDirectory is null." );
+        verifier.resetStreams();
+    }
+}
