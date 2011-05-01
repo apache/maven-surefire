fb1bf8718c044562be3cb91607c101ad724ef206
Kristian Rosenvold <krosenvold@apache.org>
2011-05-01 15:48:51 +0000

    o Let's see if this fixes the asf build
    And yes, as usual this stuff doesn't fail locally. Even with the same options ;)
    
    git-svn-id: https://svn.apache.org/repos/asf/maven/surefire/trunk@1098361 13f79535-47bb-0310-9956-ffa450edef68

--- surefire-api/src/main/java/org/apache/maven/surefire/util/NestedRuntimeException.java
+++ surefire-api/src/main/java/org/apache/maven/surefire/util/NestedRuntimeException.java
@@ -106,6 +106,7 @@ public class NestedRuntimeException
      * <p>Note that this will only check one level of nesting.
      * Use <code>getRootCause()</code> to retrieve the innermost cause.
      *
+     * @see #getRootCause()
      */
     public Throwable getCause()
     {
@@ -166,6 +167,26 @@ public class NestedRuntimeException
         {
             pw.println( this );
             getCause().printStackTrace( pw );
+        }
+    }
+
+    /**
+     * Retrieve the innermost cause of this exception, if any.
+     * <p>Currently just traverses NestedRuntimeException causes. Will use
+     * the JDK 1.4 exception cause mechanism once Spring requires JDK 1.4.
+     *
+     * @return the innermost exception, or <code>null</code> if none
+     */
+    public Throwable getRootCause()
+    {
+        Throwable cause = getCause();
+        if ( cause instanceof NestedRuntimeException )
+        {
+            return ( (NestedRuntimeException) cause ).getRootCause();
+        }
+        else
+        {
+            return cause;
         }
     }
 
