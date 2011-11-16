e6971921ad03005a78a807768d5bb78d4f0561cf
Kristian Rosenvold <krosenvold@apache.org>
2011-11-16 19:55:52 +0000

    [SUREFIRE-791] Negative run-time on failure
    Fixed with unit test
    
    git-svn-id: https://svn.apache.org/repos/asf/maven/surefire/trunk@1202863 13f79535-47bb-0310-9956-ffa450edef68

--- surefire-providers/surefire-junit47/src/main/java/org/apache/maven/surefire/junitcore/TestMethod.java
+++ surefire-providers/surefire-junit47/src/main/java/org/apache/maven/surefire/junitcore/TestMethod.java
@@ -69,6 +69,7 @@ class TestMethod
     public void testFailure( ReportEntry failure )
     {
         this.testFailure = failure;
+        setEndTime();
     }
 
     public void testError( ReportEntry failure )
---
+++ surefire-providers/surefire-junit47/src/test/java/org/apache/maven/surefire/junitcore/TestMethodTest.java
@@ -0,0 +1,41 @@
+package org.apache.maven.surefire.junitcore;
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+import junit.framework.TestCase;
+import org.apache.maven.surefire.report.ReportEntry;
+import org.apache.maven.surefire.report.SimpleReportEntry;
+
+/**
+ * @author Kristian Rosenvold
+ */
+public class TestMethodTest
+    extends TestCase
+{
+    public void testTestFailure()
+        throws Exception
+    {
+        ReportEntry reportEntry = new SimpleReportEntry( "a", "b" );
+        TestMethod testMethod = new TestMethod( reportEntry );
+        testMethod.testFailure( reportEntry );
+        final int elapsed = testMethod.getElapsed();
+        assertTrue( elapsed >= 0 );
+        assertTrue( elapsed < 100000 );
+    }
+}
