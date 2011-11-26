f246d610301e7bd7b407e6defb764342cea57a0d
Kristian Rosenvold <krosenvold@apache.org>
2011-11-26 17:31:48 +0000

    [SUREFIRE-801] Test project that demonstrates bug.
    git-svn-id: https://svn.apache.org/repos/asf/maven/surefire/trunk@1206506 13f79535-47bb-0310-9956-ffa450edef68

---
+++ surefire-integration-tests/src/test/resources/fork-mode-resource-loading/src/test/java/forkMode/ResourceLoadTest.java
@@ -0,0 +1,27 @@
+package forkMode;
+
+import junit.framework.TestCase;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URL;
+
+public class ResourceLoadTest
+    extends TestCase
+{
+
+    public void testGetResourceUrl() throws IOException {
+        final URL resource = this.getClass().getClassLoader().getResource( "myFile.txt" );
+        assertNotNull(  resource );
+    }
+
+    public void testGetResource() throws IOException {
+        final InputStream resource = this.getClass().getClassLoader().getResourceAsStream( "myFile.txt" );
+        assertNotNull(  resource );
+    }
+
+    public void testGetResourceThreadLoader() throws IOException {
+        final InputStream resource = Thread.currentThread().getContextClassLoader().getResourceAsStream( "myFile.txt" );
+        assertNotNull(  resource );
+    }
+}
