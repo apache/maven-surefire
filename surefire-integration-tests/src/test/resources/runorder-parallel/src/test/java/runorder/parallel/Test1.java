76938bad4bf67a1d8b87300d8a4a1ce9a3335159
Kristian Rosenvold <krosenvold@apache.org>
2011-12-06 17:29:36 +0000

    [SUREFIRE-793] Expanded testcase to show parallel provider misses time spent in @BeforeClass/@AfterClass
    git-svn-id: https://svn.apache.org/repos/asf/maven/surefire/trunk@1211041 13f79535-47bb-0310-9956-ffa450edef68

--- surefire-integration-tests/src/test/resources/runorder-parallel/src/test/java/runorder/parallel/Test1.java
+++ surefire-integration-tests/src/test/resources/runorder-parallel/src/test/java/runorder/parallel/Test1.java
@@ -1,33 +1,58 @@
 package runorder.parallel;
 
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
 import org.junit.Test;
+
 public class Test1
 {
-  
-    static void sleep(int ms){
-      try {
-        Thread.sleep(ms);
-      } catch (InterruptedException e) {
-        throw new RuntimeException(e);
-      }
+
+    static void sleep( int ms )
+    {
+        try
+        {
+            Thread.sleep( ms );
+        }
+        catch ( InterruptedException e )
+        {
+            throw new RuntimeException( e );
+        }
     }
 
-   @Test
-    public void testSleep200() {
-     System.out.println("Test1.sleep200 started @ " + System.currentTimeMillis());
-        sleep(200);
+    @Test
+    public void testSleep200()
+    {
+        System.out.println( "Test1.sleep200 started @ " + System.currentTimeMillis() );
+        sleep( 200 );
     }
 
-  @Test
-   public void testSleep400() {
-    System.out.println("Test1.sleep400 started @ " + System.currentTimeMillis());
-       sleep(400);
-   }
+    @Test
+    public void testSleep400()
+    {
+        System.out.println( "Test1.sleep400 started @ " + System.currentTimeMillis() );
+        sleep( 400 );
+    }
 
-  @Test
-   public void testSleep600() {
-    System.out.println("Test1.sleep600 started @ " + System.currentTimeMillis());
-       sleep(600);
-   }
+    @Test
+    public void testSleep600()
+    {
+        System.out.println( "Test1.sleep600 started @ " + System.currentTimeMillis() );
+        sleep( 600 );
+    }
+
+    @BeforeClass
+    public static void setUpBeforeClass()
+        throws Exception
+    {
+        Thread.sleep( 175 );
+    }
+
+    @AfterClass
+    public static void tearDownAfterClass()
+        throws Exception
+    {
+        Thread.sleep( 175 );
+    }
+
 
 }
