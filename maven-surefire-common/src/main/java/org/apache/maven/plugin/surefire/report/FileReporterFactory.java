6ebb0707bf877473c5af6f6502fb34262508a9c0
Herve Boutemy <hboutemy@apache.org>
2011-12-30 18:46:17 +0000

    code formatting
    git-svn-id: https://svn.apache.org/repos/asf/maven/surefire/trunk@1225891 13f79535-47bb-0310-9956-ffa450edef68

--- maven-surefire-common/src/main/java/org/apache/maven/plugin/surefire/report/FileReporterFactory.java
+++ maven-surefire-common/src/main/java/org/apache/maven/plugin/surefire/report/FileReporterFactory.java
@@ -156,7 +156,8 @@ public class FileReporterFactory
         return globalStats;
     }
 
-    public static FileReporterFactory defaultNoXml(){
+    public static FileReporterFactory defaultNoXml()
+    {
         return new FileReporterFactory( StartupReportConfiguration.defaultNoXml() );
     }
 }
