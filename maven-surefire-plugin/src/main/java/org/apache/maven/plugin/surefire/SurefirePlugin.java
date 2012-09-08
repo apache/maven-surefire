76b315c7fb5c69958515f0465c04a1d388812b90
Herve Boutemy <hboutemy@apache.org>
2012-09-08 21:16:45 +0000

    removed unused import
    git-svn-id: https://svn.apache.org/repos/asf/maven/surefire/trunk@1382368 13f79535-47bb-0310-9956-ffa450edef68

--- maven-surefire-plugin/src/main/java/org/apache/maven/plugin/surefire/SurefirePlugin.java
+++ maven-surefire-plugin/src/main/java/org/apache/maven/plugin/surefire/SurefirePlugin.java
@@ -27,7 +27,6 @@ import org.apache.maven.plugins.annotations.LifecyclePhase;
 import org.apache.maven.plugins.annotations.Mojo;
 import org.apache.maven.plugins.annotations.Parameter;
 import org.apache.maven.plugins.annotations.ResolutionScope;
-import org.apache.maven.project.MavenProject;
 import org.apache.maven.surefire.suite.RunResult;
 import org.codehaus.plexus.util.StringUtils;
 
