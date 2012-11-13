a5cda922eedca498ba62e042c8cffce4b141f3f4
Jason Dillon <jason@planet57.com>
2012-11-13 11:17:50 -0800

    Add integration tests for includesFile/excludesFile ( based on IncludesExcludesIT )

---
+++ surefire-integration-tests/src/test/resources/includes-excludes-from-file/src/test/java/org/test/DefaultTest.java
@@ -0,0 +1,15 @@
+package org.test;
+
+import java.io.FileOutputStream;
+
+public class DefaultTest
+{
+    public void testRun()
+        throws Exception
+    {
+        FileOutputStream fout = new FileOutputStream( "target/defaultTestTouchFile.txt" );
+        fout.write( '!' );
+        fout.flush();
+        fout.close();
+    }
+}
---
+++ surefire-integration-tests/src/test/resources/includes-excludes-from-file/src/test/java/org/test/NotIncludedByDefault.java
@@ -0,0 +1,15 @@
+package org.test;
+
+import java.io.FileOutputStream;
+
+public class NotIncludedByDefault
+{
+    public void testRun()
+        throws Exception
+    {
+        FileOutputStream fout = new FileOutputStream( "target/testTouchFile.txt" );
+        fout.write( '!' );
+        fout.flush();
+        fout.close();
+    }
+}
