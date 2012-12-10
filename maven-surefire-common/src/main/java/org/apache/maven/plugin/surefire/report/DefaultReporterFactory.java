90edbd4abd3697c92692a2d12f15aaf02e66af40
Kristian Rosenvold <krosenvold@apache.org>
2012-12-10 19:38:29 +0100

    o Removed NPE on odd configuration

--- maven-surefire-common/src/main/java/org/apache/maven/plugin/surefire/report/DefaultReporterFactory.java
+++ maven-surefire-common/src/main/java/org/apache/maven/plugin/surefire/report/DefaultReporterFactory.java
@@ -43,8 +43,6 @@ public class DefaultReporterFactory
 
     private final RunStatistics globalStats = new RunStatistics();
 
-    private final ConsoleReporter multicastingReporter;
-
     private final StartupReportConfiguration reportConfiguration;
 
     private final StatisticsReporter statisticsReporter;
@@ -55,7 +53,6 @@ public class DefaultReporterFactory
     public DefaultReporterFactory( StartupReportConfiguration reportConfiguration )
     {
         this.reportConfiguration = reportConfiguration;
-        multicastingReporter = reportConfiguration.instantiateConsoleReporter();
         this.statisticsReporter = reportConfiguration.instantiateStatisticsReporter();
         runStarting();
     }
@@ -106,7 +103,7 @@ public class DefaultReporterFactory
         logger.info( "" );
         if ( globalStats.hadFailures() )
         {
-            multicastingReporter.writeMessage( "Failed tests: " );
+            logger.info( "Failed tests: " );
             for ( Object o : this.globalStats.getFailureSources() )
             {
                 logger.info( "  " + o );
