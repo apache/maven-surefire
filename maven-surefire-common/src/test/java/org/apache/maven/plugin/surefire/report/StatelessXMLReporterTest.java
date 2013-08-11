6d9fe077ad30cc4fc8cb22efb0e091b2df5d4321
Andreas Gudian <agudian@apache.org>
2013-08-11 11:43:12 +0200

    o Remove unused import

--- maven-surefire-common/src/test/java/org/apache/maven/plugin/surefire/report/StatelessXMLReporterTest.java
+++ maven-surefire-common/src/test/java/org/apache/maven/plugin/surefire/report/StatelessXMLReporterTest.java
@@ -23,8 +23,6 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.nio.ByteBuffer;
-import java.nio.charset.Charset;
 
 import junit.framework.AssertionFailedError;
 import junit.framework.TestCase;
