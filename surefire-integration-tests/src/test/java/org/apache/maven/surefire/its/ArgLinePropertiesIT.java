4683d4d1a208a30ff3beb24c2da93d8b3674da7b
Henning Schmiedehausen <henning@schmiedehausen.org>
2013-12-18 19:37:27 -0800

    Integration test for properties replacement.

---
+++ surefire-integration-tests/src/test/java/org/apache/maven/surefire/its/ArgLinePropertiesIT.java
@@ -0,0 +1,39 @@
+package org.apache.maven.surefire.its;
+
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+import org.apache.maven.surefire.its.fixture.SurefireJUnit4IntegrationTestCase;
+import org.junit.Test;
+
+/**
+ * Test passing an argLine parameter
+ *
+ * @author <a href="mailto:dfabulich@apache.org">Dan Fabulich</a>
+ * @author <a href="mailto:krosenvold@apache.org">Kristian Rosenvold</a>
+ */
+public class ArgLinePropertiesIT
+    extends SurefireJUnit4IntegrationTestCase
+{
+    @Test
+    public void argLine()
+    {
+        unpack( "/argLine-properties" ).executeTest().verifyErrorFree( 4 );
+    }
+}
