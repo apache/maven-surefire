07742db44f2777f6b0e89fe1e6c33ed5b24608b9
tibordigana <tibor17@lycos.com>
2014-10-08 20:36:33 +0200

    [SUREFIRE-797] Parallel junit does not run in parallel when a Suite is used at the top level

--- surefire-integration-tests/src/test/java/org/apache/maven/surefire/its/jiras/Surefire747MethodParallelWithSuiteCountIT.java
+++ surefire-integration-tests/src/test/java/org/apache/maven/surefire/its/jiras/Surefire747MethodParallelWithSuiteCountIT.java
@@ -19,9 +19,19 @@ package org.apache.maven.surefire.its.jiras;
  * under the License.
  */
 
+import org.apache.maven.it.VerificationException;
+import org.apache.maven.surefire.its.fixture.OutputValidator;
 import org.apache.maven.surefire.its.fixture.SurefireJUnit4IntegrationTestCase;
 import org.apache.maven.surefire.its.fixture.SurefireLauncher;
 import org.junit.Test;
+
+import java.util.Iterator;
+import java.util.Set;
+import java.util.TreeSet;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+import static org.junit.Assert.assertTrue;
 
 /**
  * @author Kristian Rosenvold
@@ -30,21 +40,72 @@ public class Surefire747MethodParallelWithSuiteCountIT
     extends SurefireJUnit4IntegrationTestCase
 {
 
+    private static Set<String> printTestLines( OutputValidator validator, String pattern )
+        throws VerificationException
+    {
+        Set<String> log = new TreeSet<String>( validator.loadLogLines() );
+        for ( Iterator<String> it = log.iterator(); it.hasNext(); )
+        {
+            String line = it.next();
+            if ( !line.contains( pattern ) )
+            {
+                it.remove();
+            }
+        }
+        return log;
+    }
+
+    private static long duration( String logLine )
+    {
+        return Integer.decode( logLine.split( "=" )[1] );
+    }
+
     @Test
     public void testMethodsParallelWithSuite()
+        throws VerificationException
     {
-        unpack().executeTest().verifyErrorFree( 6 );
+        OutputValidator validator = unpack().executeTest().verifyErrorFree( 6 );
+        Set<String> testLines = printTestLines( validator, "test finished after duration=" );
+        assertThat( testLines.size(), is( 2 ) );
+        for ( String testLine : testLines )
+        {
+            long duration = duration( testLine );
+            long min = 250, max = 750;
+            assertTrue( String.format( "duration %d should be between %d and %d millis", duration, min, max ),
+                        duration > min && duration < max );
+        }
+        Set<String> suiteLines = printTestLines( validator, "suite finished after duration=" );
+        assertThat( suiteLines.size(), is( 1 ) );
+        long duration = duration( suiteLines.iterator().next() );
+        long min = 750, max = 1250;
+        assertTrue( String.format( "duration %d should be between %d and %d millis", duration, min, max ),
+                    duration > min && duration < max );
     }
 
     @Test
     public void testClassesParallelWithSuite()
+        throws VerificationException
     {
-        unpack().parallelClasses().executeTest().verifyErrorFree( 6 );
+        OutputValidator validator = unpack().parallelClasses().executeTest().verifyErrorFree( 6 );
+        Set<String> testLines = printTestLines( validator, "test finished after duration=" );
+        assertThat( testLines.size(), is( 2 ) );
+        for ( String testLine : testLines )
+        {
+            long duration = duration( testLine );
+            long min = 1250, max = 1750;
+            assertTrue( String.format( "duration %d should be between %d and %d millis", duration, min, max ),
+                        duration > min && duration < max );
+        }
+        Set<String> suiteLines = printTestLines( validator, "suite finished after duration=" );
+        assertThat( suiteLines.size(), is( 1 ) );
+        long duration = duration( suiteLines.iterator().next() );
+        long min = 1250, max = 1750;
+        assertTrue( String.format( "duration %d should be between %d and %d millis", duration, min, max ),
+                    duration > min && duration < max );
     }
 
     public SurefireLauncher unpack()
     {
         return unpack( "junit47-parallel-with-suite" );
     }
-
 }
--- surefire-integration-tests/src/test/resources/junit47-parallel-with-suite/src/test/java/surefire747/TestSuite.java
+++ surefire-integration-tests/src/test/resources/junit47-parallel-with-suite/src/test/java/surefire747/TestSuite.java
@@ -19,6 +19,8 @@ package surefire747;
  * under the License.
  */
 
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 
@@ -33,4 +35,18 @@ import org.junit.runners.Suite;
 })
 public class TestSuite
 {
+    private static long startedAt;
+
+    @BeforeClass
+    public static void beforeClass()
+    {
+        startedAt = System.currentTimeMillis();
+    }
+
+    @AfterClass
+    public static void afterClass()
+    {
+        System.out.println( String.format( "%s suite finished after duration=%d", TestSuite.class.getSimpleName(),
+                                           System.currentTimeMillis() - startedAt ) );
+    }
 }
