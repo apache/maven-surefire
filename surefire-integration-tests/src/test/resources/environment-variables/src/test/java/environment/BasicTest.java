26982d70b4cfe4880647c58e24cbc45a626d5a1c
cdeneux <christophe.deneux@gmail.com>
2014-10-08 14:13:27 +0200

    [SUREFIRE-963] Unable to set empty environment variables
    Fix SUREFIRE-963
    
    Unit test about env variable values completed with the use case of an
    env varaible not defined
    
    Apply the maven code style to the previous modifications

--- surefire-integration-tests/src/test/resources/environment-variables/src/test/java/environment/BasicTest.java
+++ surefire-integration-tests/src/test/resources/environment-variables/src/test/java/environment/BasicTest.java
@@ -19,11 +19,13 @@ package environment;
  * under the License.
  */
 
+import org.hamcrest.core.IsNull;
 import org.junit.Assert;
 import org.junit.Test;
 
 import static org.hamcrest.core.Is.is;
 import static org.hamcrest.core.IsNull.notNullValue;
+import static org.hamcrest.core.IsNull.nullValue;
 
 
 public class BasicTest
@@ -35,6 +37,9 @@ public class BasicTest
     {
         Assert.assertThat( System.getenv( "PATH" ), notNullValue() );
         Assert.assertThat( System.getenv( "DUMMY_ENV_VAR" ), is( "foo" ) );
+        Assert.assertThat( System.getenv( "EMPTY_VAR" ), is( "" ) );
+        Assert.assertThat( System.getenv( "UNSET_VAR" ), is( "" ) );
+        Assert.assertThat( System.getenv( "UNDEFINED_VAR" ), nullValue() );
     }
 
 
