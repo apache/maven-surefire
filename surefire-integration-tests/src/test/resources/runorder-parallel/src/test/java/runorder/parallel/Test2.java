710a3d58d62bae40a9bf2bed38c3877d4458be8e
Tibor17 <tibor17@lycos.com>
2015-04-30 23:54:56 +0200

    [SUREFIRE] reformat code

--- surefire-integration-tests/src/test/resources/runorder-parallel/src/test/java/runorder/parallel/Test2.java
+++ surefire-integration-tests/src/test/resources/runorder-parallel/src/test/java/runorder/parallel/Test2.java
@@ -24,23 +24,27 @@ import org.junit.Test;
 /**
  * @author Kristian Rosenvold
  */
-public class Test2 {
+public class Test2
+{
 
-  @Test
-  public void testSleep100() {
-    System.out.println("Test2.sleep100 started @ " + System.currentTimeMillis());
-    Test1.sleep(100);
-  }
+    @Test
+    public void testSleep100()
+    {
+        System.out.println( "Test2.sleep100 started @ " + System.currentTimeMillis() );
+        Test1.sleep( 100 );
+    }
 
-  @Test
-  public void testSleep300() {
-    System.out.println("Test2.sleep300 started @ " + System.currentTimeMillis());
-    Test1.sleep(300);
-  }
+    @Test
+    public void testSleep300()
+    {
+        System.out.println( "Test2.sleep300 started @ " + System.currentTimeMillis() );
+        Test1.sleep( 300 );
+    }
 
-  @Test
-  public void testSleep500() {
-    System.out.println("Test2.sleep500 started @ " + System.currentTimeMillis());
-    Test1.sleep(500);
-  }
+    @Test
+    public void testSleep500()
+    {
+        System.out.println( "Test2.sleep500 started @ " + System.currentTimeMillis() );
+        Test1.sleep( 500 );
+    }
 }
--- surefire-integration-tests/src/test/resources/runorder-parallel/src/test/java/runorder/parallel/Test3.java
+++ surefire-integration-tests/src/test/resources/runorder-parallel/src/test/java/runorder/parallel/Test3.java
@@ -26,39 +26,48 @@ import org.junit.Test;
 /**
  * @author Kristian Rosenvold
  */
-public class Test3 {
+public class Test3
+{
 
-  private void sleep(int ms) {
-    try {
-      Thread.sleep(ms);
-    } catch (InterruptedException e) {
-      throw new RuntimeException(e);
+    private void sleep( int ms )
+    {
+        try
+        {
+            Thread.sleep( ms );
+        }
+        catch ( InterruptedException e )
+        {
+            throw new RuntimeException( e );
+        }
     }
-  }
 
-  @Test
-  public void testSleep10() {
-    System.out.println("Test3.sleep10 started @ " + System.currentTimeMillis());
-    Test1.sleep(10);
-  }
+    @Test
+    public void testSleep10()
+    {
+        System.out.println( "Test3.sleep10 started @ " + System.currentTimeMillis() );
+        Test1.sleep( 10 );
+    }
 
-  @Test
-  public void testSleep30() {
-    System.out.println("Test3.sleep30 started @ " + System.currentTimeMillis());
-    Test1.sleep(30);
-  }
+    @Test
+    public void testSleep30()
+    {
+        System.out.println( "Test3.sleep30 started @ " + System.currentTimeMillis() );
+        Test1.sleep( 30 );
+    }
 
-  @Test
-  public void testSleep50() {
-    System.out.println("Test3.sleep50 started @ " + System.currentTimeMillis());
-    Test1.sleep(50);
-  }
+    @Test
+    public void testSleep50()
+    {
+        System.out.println( "Test3.sleep50 started @ " + System.currentTimeMillis() );
+        Test1.sleep( 50 );
+    }
 
     @BeforeClass
     public static void setUpBeforeClass()
         throws Exception
     {
-        System.out.println( Thread.currentThread().getName() + " Test3 beforeClass sleep 175 " + System.currentTimeMillis() );
+        System.out.println(
+            Thread.currentThread().getName() + " Test3 beforeClass sleep 175 " + System.currentTimeMillis() );
         Thread.sleep( 175 );
     }
 
@@ -66,7 +75,8 @@ public class Test3 {
     public static void tearDownAfterClass()
         throws Exception
     {
-        System.out.println( Thread.currentThread().getName() + " Test3 afterClass sleep 175 " + System.currentTimeMillis() );
+        System.out.println(
+            Thread.currentThread().getName() + " Test3 afterClass sleep 175 " + System.currentTimeMillis() );
         Thread.sleep( 175 );
     }
 
