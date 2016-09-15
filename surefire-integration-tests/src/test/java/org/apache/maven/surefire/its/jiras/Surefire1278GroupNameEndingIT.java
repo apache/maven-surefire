82714f8349b9e790fac18e9d913f4dfb613de3f0
Kostya Hospodarysko <khospodarysko@lohika.com>
2016-09-16 01:08:42 +0300

    Fix for SUREFIRE-1278 - TestNG tests are run with group name that ends with specified group.
    Configured pom.xml to run only group "group" in order to show the issue.
    
    Added IT test class.

---
+++ surefire-integration-tests/src/test/java/org/apache/maven/surefire/its/jiras/Surefire1278GroupNameEndingIT.java
@@ -0,0 +1,44 @@
+package org.apache.maven.surefire.its.jiras;
+
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+import org.apache.maven.surefire.its.fixture.SurefireJUnit4IntegrationTestCase;
+import org.apache.maven.surefire.its.fixture.SurefireLauncher;
+import org.junit.Test;
+
+/**
+ * Test the group filter for TestNG
+ *
+ */
+public class Surefire1278GroupNameEndingIT
+    extends SurefireJUnit4IntegrationTestCase
+{
+    @Test
+    public void testOnlyGroups()
+    {
+        unpack().setGroups( "group" ).executeTest().verifyErrorFree( 1 );
+    }
+
+    public SurefireLauncher unpack()
+    {
+        return unpack( "/surefire-1278-group-name-ending" );
+    }
+
+}
