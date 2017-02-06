73e09a8b2b245e76e072c1809e7fa50be02e95b6
Benedikt Ritter <britter@apache.org>
2017-02-06 14:02:48 +0100

    SUREFIRE-1330: Import code from https://issues.apache.org/jira/browse/SUREFIRE-1330 unchanged as provided

---
+++ surefire-providers/surefire-junit5/src/main/java/org/junit/platform/surefire/provider/RunListenerAdapter.java
@@ -0,0 +1,124 @@
+/*
+ * Copyright 2015-2017 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.junit.platform.surefire.provider;
+
+import static org.apache.maven.surefire.report.SimpleReportEntry.ignored;
+import static org.junit.platform.engine.TestExecutionResult.Status.ABORTED;
+import static org.junit.platform.engine.TestExecutionResult.Status.FAILED;
+
+import java.util.Optional;
+
+import org.apache.maven.surefire.report.PojoStackTraceWriter;
+import org.apache.maven.surefire.report.RunListener;
+import org.apache.maven.surefire.report.SimpleReportEntry;
+import org.apache.maven.surefire.report.StackTraceWriter;
+import org.junit.platform.engine.TestExecutionResult;
+import org.junit.platform.engine.TestSource;
+import org.junit.platform.engine.support.descriptor.ClassSource;
+import org.junit.platform.engine.support.descriptor.MethodSource;
+import org.junit.platform.launcher.TestExecutionListener;
+import org.junit.platform.launcher.TestIdentifier;
+import org.junit.platform.launcher.TestPlan;
+
+/**
+ * @since 1.0
+ */
+final class RunListenerAdapter implements TestExecutionListener {
+
+	private final RunListener runListener;
+	private Optional<TestPlan> testPlan = Optional.empty();
+
+	public RunListenerAdapter(RunListener runListener) {
+		this.runListener = runListener;
+	}
+
+	@Override
+	public void testPlanExecutionStarted(TestPlan testPlan) {
+		this.testPlan = Optional.of(testPlan);
+	}
+
+	@Override
+	public void testPlanExecutionFinished(TestPlan testPlan) {
+		this.testPlan = Optional.empty();
+	}
+
+	@Override
+	public void executionStarted(TestIdentifier testIdentifier) {
+		if (testIdentifier.isTest()) {
+			runListener.testStarting(createReportEntry(testIdentifier, Optional.empty()));
+		}
+	}
+
+	@Override
+	public void executionSkipped(TestIdentifier testIdentifier, String reason) {
+		String source = getClassName(testIdentifier).orElseGet(() -> parentDisplayName(testIdentifier));
+		runListener.testSkipped(ignored(source, testIdentifier.getDisplayName(), reason));
+	}
+
+	@Override
+	public void executionFinished(TestIdentifier testIdentifier, TestExecutionResult testExecutionResult) {
+		if (testExecutionResult.getStatus() == ABORTED) {
+			runListener.testAssumptionFailure(createReportEntry(testIdentifier, testExecutionResult.getThrowable()));
+		}
+		else if (testExecutionResult.getStatus() == FAILED) {
+			runListener.testFailed(createReportEntry(testIdentifier, testExecutionResult.getThrowable()));
+		}
+		else if (testIdentifier.isTest()) {
+			runListener.testSucceeded(createReportEntry(testIdentifier, Optional.empty()));
+		}
+	}
+
+	private SimpleReportEntry createReportEntry(TestIdentifier testIdentifier, Optional<Throwable> throwable) {
+		Optional<String> className = getClassName(testIdentifier);
+		if (className.isPresent()) {
+			StackTraceWriter traceWriter = new PojoStackTraceWriter(className.get(),
+				getMethodName(testIdentifier).orElse(""), throwable.orElse(null));
+			return new SimpleReportEntry(className.get(), testIdentifier.getDisplayName(), traceWriter, null);
+		}
+		else {
+			return new SimpleReportEntry(parentDisplayName(testIdentifier), testIdentifier.getDisplayName(), null);
+		}
+	}
+
+	private Optional<String> getClassName(TestIdentifier testIdentifier) {
+		TestSource testSource = testIdentifier.getSource().orElse(null);
+		if (testSource instanceof ClassSource) {
+			return Optional.of(((ClassSource) testSource).getJavaClass().getName());
+		}
+		if (testSource instanceof MethodSource) {
+			return Optional.of(((MethodSource) testSource).getClassName());
+		}
+		return Optional.empty();
+	}
+
+	private Optional<String> getMethodName(TestIdentifier testIdentifier) {
+		TestSource testSource = testIdentifier.getSource().orElse(null);
+		if (testSource instanceof MethodSource) {
+			return Optional.of(((MethodSource) testSource).getMethodName());
+		}
+		return Optional.empty();
+	}
+
+	private String parentDisplayName(TestIdentifier testIdentifier) {
+		// @formatter:off
+		return testPlan
+			.flatMap(plan -> plan.getParent(testIdentifier))
+			.map(TestIdentifier::getDisplayName)
+			.orElseGet(testIdentifier::getUniqueId);
+		// @formatter:on
+	}
+}
---
+++ surefire-providers/surefire-junit5/src/test/java/org/junit/platform/surefire/provider/TestPlanScannerFilterTests.java
@@ -0,0 +1,157 @@
+/*
+ * Copyright 2015-2017 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.junit.platform.surefire.provider;
+
+import static java.util.Collections.emptyList;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import java.util.List;
+import java.util.stream.Stream;
+
+import org.junit.jupiter.api.DynamicTest;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestFactory;
+import org.junit.platform.engine.Filter;
+import org.junit.platform.launcher.core.LauncherFactory;
+
+/**
+ * Unit tests for {@link TestPlanScannerFilter}.
+ *
+ * @since 1.0
+ */
+public class TestPlanScannerFilterTests {
+
+	@Test
+	void emptyClassAccepted() {
+		assertTrue(newFilter().accept(EmptyClass.class), "accepts empty class because it is a container");
+	}
+
+	@Test
+	void classWithNoTestMethodsIsAccepted() {
+		assertTrue(newFilter().accept(ClassWithMethods.class),
+			"accepts class with no @Test methods because it is a container");
+	}
+
+	@Test
+	void classWithTestMethodsIsAccepted() {
+		assertTrue(newFilter().accept(ClassWithTestMethods.class));
+	}
+
+	@Test
+	void classWithNestedTestClassIsAccepted() {
+		assertTrue(newFilter().accept(ClassWithNestedTestClass.class));
+	}
+
+	@Test
+	void classWithDeeplyNestedTestClassIsAccepted() {
+		assertTrue(newFilter().accept(ClassWithDeeplyNestedTestClass.class));
+	}
+
+	@Test
+	void classWithTestFactoryIsAccepted() {
+		assertTrue(newFilter().accept(ClassWithTestFactory.class));
+	}
+
+	@Test
+	void classWithNestedTestFactoryIsAccepted() {
+		assertTrue(newFilter().accept(ClassWithNestedTestFactory.class));
+	}
+
+	private TestPlanScannerFilter newFilter() {
+		return new TestPlanScannerFilter(LauncherFactory.create(), new Filter<?>[0]);
+	}
+
+	private static class EmptyClass {
+	}
+
+	@SuppressWarnings("unused")
+	private static class ClassWithMethods {
+
+		void method1() {
+		}
+
+		void method2() {
+		}
+	}
+
+	private static class ClassWithTestMethods {
+
+		@Test
+		void test1() {
+		}
+
+		@Test
+		public void test2() {
+		}
+	}
+
+	private static class ClassWithNestedTestClass {
+
+		@SuppressWarnings("unused")
+		void method() {
+		}
+
+		@Nested
+		class TestClass {
+
+			@Test
+			void test1() {
+			}
+		}
+	}
+
+	private static class ClassWithDeeplyNestedTestClass {
+
+		@Nested
+		class Level1 {
+
+			@Nested
+			class Level2 {
+
+				@Nested
+				class TestClass {
+
+					@Test
+					void test1() {
+					}
+				}
+			}
+		}
+	}
+
+	private static class ClassWithTestFactory {
+
+		@TestFactory
+		Stream<DynamicTest> tests() {
+			return Stream.empty();
+		}
+	}
+
+	private static class ClassWithNestedTestFactory {
+
+		@Nested
+		class TestClass {
+
+			@TestFactory
+			List<DynamicTest> tests() {
+				return emptyList();
+			}
+		}
+	}
+
+}
