0bbbe354ea101502ef3074405fd59970f5a239f4
Davide Angelocola <davide.angelocola@gmail.com>
2019-04-03 00:11:24 +0200

    [SUREFIRE-1432] trimStackTrace = false by default

--- maven-surefire-common/src/main/java/org/apache/maven/plugin/surefire/AbstractSurefireMojo.java
+++ maven-surefire-common/src/main/java/org/apache/maven/plugin/surefire/AbstractSurefireMojo.java
@@ -656,7 +656,7 @@ public abstract class AbstractSurefireMojo
      *
      * @since 2.2
      */
-    @Parameter( property = "trimStackTrace", defaultValue = "true" )
+    @Parameter( property = "trimStackTrace", defaultValue = "false" )
     private boolean trimStackTrace;
 
     /**
