c51267081ff786d66a8f479aeab32d3923e1589b
tibordigana <tibordigana@apache.org>
2019-10-02 21:34:21 +0200

    temporarily disabled JUnit5 5.6.0-SNAPSHOT and 1.6.0-SNAPSHOT due to Sonatype issue OSSRH-51220

--- surefire-its/src/test/java/org/apache/maven/surefire/its/JUnitPlatformEnginesIT.java
+++ surefire-its/src/test/java/org/apache/maven/surefire/its/JUnitPlatformEnginesIT.java
@@ -68,7 +68,7 @@ public class JUnitPlatformEnginesIT
         args.add( new Object[] { "1.5.0-RC1", "5.5.0-RC1", "1.2.0", "1.1.0" } );
         args.add( new Object[] { "1.5.0", "5.5.0", "1.2.0", "1.1.0" } );
         args.add( new Object[] { "1.5.2", "5.5.2", "1.2.0", "1.1.0" } );
-        args.add( new Object[] { "1.6.0-SNAPSHOT", "5.6.0-SNAPSHOT", "1.2.0", "1.1.0" } );
+        //args.add( new Object[] { "1.6.0-SNAPSHOT", "5.6.0-SNAPSHOT", "1.2.0", "1.1.0" } );
         return args;
     }
 
