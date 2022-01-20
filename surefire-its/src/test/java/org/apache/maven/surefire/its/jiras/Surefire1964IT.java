505d862a62594b16ebcd81da68c59d2cd4f29852
Tibor Digaňa <tibordigana@apache.org>
2022-01-20 13:04:20 +0100

    added ITs

---
+++ surefire-its/src/test/java/org/apache/maven/surefire/its/jiras/Surefire1964IT.java
@@ -0,0 +1,55 @@
+package org.apache.maven.surefire.its.jiras;
+
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+import org.apache.maven.surefire.its.fixture.SurefireJUnit4IntegrationTestCase;
+import org.junit.Test;
+
+import static org.hamcrest.Matchers.containsString;
+import static org.hamcrest.Matchers.is;
+
+/**
+ *
+ */
+public class Surefire1964IT extends SurefireJUnit4IntegrationTestCase
+{
+    @Test
+    public void shouldFilterTests() throws Exception
+    {
+        unpack( "surefire-1964" )
+            .executeTest()
+            .assertTestSuiteResults( 1 )
+            .assertThatLogLine( containsString( "executed testXYZ" ), is( 1 ) )
+            .assertThatLogLine( containsString( "executed testABC" ), is( 0 ) )
+            .assertThatLogLine( containsString( "executed dontRun" ), is( 0 ) );
+    }
+
+    @Test
+    public void shouldFilterTestsInPluginProcess() throws Exception
+    {
+        unpack( "surefire-1964" )
+            .forkCount( 0 )
+            .executeTest()
+            .assertTestSuiteResults( 1 )
+            .assertThatLogLine( containsString( "executed testXYZ" ), is( 1 ) )
+            .assertThatLogLine( containsString( "executed testABC" ), is( 0 ) )
+            .assertThatLogLine( containsString( "executed dontRun" ), is( 0 ) );
+    }
+}
