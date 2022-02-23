778de43fddb6a038575393b651213eab0eb16bc5
Slawomir Jaranowski <s.jaranowski@gmail.com>
2022-02-23 22:09:33 +0100

    [SUREFIRE-2026] Improve assertions in Surefire1787JUnit5IT

--- surefire-its/src/test/resources/junit5-runner/src/test/java/examples/b/BTest.java
+++ surefire-its/src/test/resources/junit5-runner/src/test/java/examples/b/BTest.java
@@ -2,9 +2,13 @@ package examples.b;
 
 import org.junit.Test;
 
-public class BTest {
-    @Test
-    public void test() {
+import static org.junit.Assert.fail;
 
+public class BTest
+{
+    @Test
+    public void test()
+    {
+        fail();
     }
 }
