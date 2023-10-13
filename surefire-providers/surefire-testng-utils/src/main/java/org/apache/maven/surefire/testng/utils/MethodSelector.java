5a0943805f7ef3da691bd4f39660346bfc7283d6
Javier A. Ortiz Bultron <javier.ortiz.78@gmail.com>
2023-10-13 09:59:38 -0500

    [SUREFIRE-2202] NullPointerException if super class meets specific condition
    This closes #673

--- surefire-providers/surefire-testng-utils/src/main/java/org/apache/maven/surefire/testng/utils/MethodSelector.java
+++ surefire-providers/surefire-testng-utils/src/main/java/org/apache/maven/surefire/testng/utils/MethodSelector.java
@@ -61,7 +61,9 @@ public class MethodSelector implements IMethodSelector {
         boolean hasTestResolver = resolver != null && !resolver.isEmpty();
         if (hasTestResolver) {
             boolean run = false;
-            for (Class<?> clazz = test.getRealClass(); !run && clazz != Object.class; clazz = clazz.getSuperclass()) {
+            for (Class<?> clazz = test.getRealClass();
+                    !run && clazz != null && clazz != Object.class;
+                    clazz = clazz.getSuperclass()) {
                 run = resolver.shouldRun(clazz, test.getMethodName());
             }
             return run;
