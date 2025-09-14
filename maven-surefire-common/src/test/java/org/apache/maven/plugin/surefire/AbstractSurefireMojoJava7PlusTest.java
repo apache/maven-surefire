18223b45996df8bd80c5fefca1829182995d555d
Olivier Lamy <olamy@apache.org>
2025-09-14 21:37:24 +1000

    use junit plaform runner for junit 4.4
    Signed-off-by: Olivier Lamy <olamy@apache.org>

--- maven-surefire-common/src/test/java/org/apache/maven/plugin/surefire/AbstractSurefireMojoJava7PlusTest.java
+++ maven-surefire-common/src/test/java/org/apache/maven/plugin/surefire/AbstractSurefireMojoJava7PlusTest.java
@@ -538,7 +538,7 @@ public class AbstractSurefireMojoJava7PlusTest {
         }
 
         ProviderInfo newJUnitPlatformProviderInfo() {
-            return new JUnitPlatformProviderInfo(null, null, null);
+            return new JUnitPlatformProviderInfo(null, null, null, null, null);
         }
 
         @Override
