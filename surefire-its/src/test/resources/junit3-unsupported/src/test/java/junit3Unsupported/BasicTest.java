d67e3d6401e0ec97854b85b744eb2a30dd8678e2
sebtiem <setie@mailbox.org>
2025-09-26 10:57:47 +0200

    remove unnecessary packages from test

--- surefire-its/src/test/resources/junit3-unsupported/src/test/java/junit3Unsupported/BasicTest.java
+++ surefire-its/src/test/resources/junit3-unsupported/src/test/java/junit3Unsupported/BasicTest.java
@@ -1,7 +1,5 @@
 package junit3Unsupported;
 
-import static org.hamcrest.core.Is.*;
-import static org.hamcrest.core.IsNull.*;
 import junit.framework.TestCase;
 
 public class BasicTest extends TestCase
