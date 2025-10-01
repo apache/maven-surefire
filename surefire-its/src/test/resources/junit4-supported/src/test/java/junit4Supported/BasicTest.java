790bc23631ae7013cfd193d27527ef09c57eff79
sebtiem <setie@mailbox.org>
2025-10-01 15:36:36 +0200

    use version range, add tests for JUnit 4 versions, adjust naming of similar JUnit 3 test

---
+++ surefire-its/src/test/resources/junit4-supported/src/test/java/junit4Supported/BasicTest.java
@@ -0,0 +1,14 @@
+package junit4Unsupported;
+
+import junit.framework.TestCase;
+
+public class BasicTest extends TestCase
+{
+
+    public void testEnvVar()
+    {
+        assertEquals(1, 1);
+    }
+
+
+}
