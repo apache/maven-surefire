208b25904f73fbf5407f2072fd2cdd07f4e3d577
Olivier Lamy <olamy@apache.org>
2025-10-18 19:13:17 +1000

    fix test ng version check, ensure order by last modified value from file
    Signed-off-by: Olivier Lamy <olamy@apache.org>

--- surefire-its/src/test/java/org/apache/maven/surefire/its/CheckTestNgVersionsIT.java
+++ surefire-its/src/test/java/org/apache/maven/surefire/its/CheckTestNgVersionsIT.java
@@ -53,12 +53,22 @@ public class CheckTestNgVersionsIT extends SurefireJUnit4IntegrationTestCase {
 
     @Test
     public void test69136() {
-        runTestNgTestWithRunOrder("6.9.13.6");
+        unpack("testng-simple")
+                .sysProp("testNgVersion", "6.9.13.6")
+                .maven()
+                .withFailure()
+                .executeTest()
+                .verifyTextInLog("TestNG support requires version 6.14.3 or above");
     }
 
     @Test
     public void test6821() {
-        runTestNgTestWithRunOrder("6.8.21");
+        unpack("testng-simple")
+                .sysProp("testNgVersion", "6.8.21")
+                .maven()
+                .withFailure()
+                .executeTest()
+                .verifyTextInLog("TestNG support requires version 6.14.3 or above");
     }
 
     private void runTestNgTestWithRunOrder(String version) {
@@ -87,7 +97,7 @@ public class CheckTestNgVersionsIT extends SurefireJUnit4IntegrationTestCase {
             assertEquals(3, report.size());
 
             assertTrue(
-                    "TestNGSuiteTestC was executed first",
+                    "TestNGSuiteTestC was not executed first",
                     getTestClass(report, 0).endsWith("TestNGSuiteTestC"));
 
             assertTrue(
