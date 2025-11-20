75a99d07f4d2d1ad1aba213352907d23d05dae96
Olivier Lamy <olamy@apache.org>
2025-11-20 22:24:12 +1000

    fix bad change
    Signed-off-by: Olivier Lamy <olamy@apache.org>

--- surefire-its/src/test/java/org/apache/maven/surefire/its/fixture/MavenLauncher.java
+++ surefire-its/src/test/java/org/apache/maven/surefire/its/fixture/MavenLauncher.java
@@ -268,9 +268,6 @@ public final class MavenLauncher {
             getVerifier().execute();
             return getValidator();
         } catch (VerificationException e) {
-            if (expectFailure) {
-                return getValidator();
-            }
             throw new SurefireVerifierException(e.getLocalizedMessage(), e);
         }
     }
