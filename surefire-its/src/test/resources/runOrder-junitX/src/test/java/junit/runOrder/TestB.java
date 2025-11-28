9d53dca927631efaa0791b71e938dbccfba03542
Olivier Lamy <olamy@apache.org>
2025-11-28 19:20:45 +1000

    add runOrder test for junit 5 and 6
    Signed-off-by: Olivier Lamy <olamy@apache.org>

---
+++ surefire-its/src/test/resources/runOrder-junitX/src/test/java/junit/runOrder/TestB.java
@@ -0,0 +1,11 @@
+package junit.runOrder;
+
+import org.junit.jupiter.api.Test;
+public class TestB
+{
+    @Test
+    public void testTwo()
+    {
+        System.out.println( "TB" );
+    }
+}
---
+++ surefire-its/src/test/resources/runOrder-junitX/src/test/java/junit/runOrder/TestC.java
@@ -0,0 +1,12 @@
+package junit.runOrder;
+
+import org.junit.jupiter.api.Test;
+
+public class TestC
+{
+    @Test
+    public void testTwo()
+    {
+        System.out.println( "TC" );
+    }
+}
