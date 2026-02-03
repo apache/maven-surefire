ae048972b8b154b085864aea990f52c2fa841eaf
Olivier Lamy <olamy@apache.org>
2026-02-04 08:14:17 +1000

    avoid not needed rename for now
    Signed-off-by: Olivier Lamy <olamy@apache.org>

---
+++ surefire-its/src/test/java/org/apache/maven/surefire/its/jiras/Surefire1364SystemPropertiesIT.java
@@ -0,0 +1,91 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package org.apache.maven.surefire.its.jiras;
+
+import org.apache.maven.surefire.its.fixture.OutputValidator;
+import org.apache.maven.surefire.its.fixture.SurefireJUnit4IntegrationTestCase;
+import org.apache.maven.surefire.its.fixture.SurefireLauncher;
+import org.junit.Test;
+
+/**
+ * Report XML should contain system properties of forked JVM.
+ *
+ * @author <a href="mailto:tibordigana@apache.org">Tibor Digana (tibor17)</a>
+ * @since 2.20.1
+ */
+public class Surefire1364SystemPropertiesIT extends SurefireJUnit4IntegrationTestCase {
+
+    @Test
+    public void junit4Forked() {
+        SurefireLauncher launcher = unpack("surefire-1364");
+        OutputValidator validator =
+                launcher.setForkJvm().forkOnce().executeTest().verifyErrorFree(2);
+
+        validator
+                .getSurefireReportsXmlFile("TEST-FirstTest.xml")
+                .assertContainsText("<property name=\"forkedProp\" value=\"forkedValue1\"/>");
+
+        validator
+                .getSurefireReportsXmlFile("TEST-SecondTest.xml")
+                .assertContainsText("<property name=\"forkedProp\" value=\"forkedValue1\"/>");
+    }
+
+    @Test
+    public void junit4InProcess() {
+        SurefireLauncher launcher = unpack("surefire-1364");
+        OutputValidator validator =
+                launcher.setForkJvm().forkNever().executeTest().verifyErrorFree(2);
+
+        validator
+                .getSurefireReportsXmlFile("TEST-FirstTest.xml")
+                .assertContainsText("<property name=\"forkedProp\" value=\"forkedValue1\"/>");
+
+        validator
+                .getSurefireReportsXmlFile("TEST-SecondTest.xml")
+                .assertContainsText("<property name=\"forkedProp\" value=\"forkedValue1\"/>");
+    }
+
+    @Test
+    public void testNg() {
+        SurefireLauncher launcher = unpack("surefire-1364");
+        OutputValidator validator = launcher.setForkJvm()
+                .activateProfile("testng")
+                .forkOnce()
+                .executeTest()
+                .verifyErrorFree(3);
+
+        validator
+                .getSurefireReportsXmlFile("TEST-FirstTest.xml")
+                .assertContainsText("<property name=\"forkedProp\" value=\"forkedValue1\"/>");
+    }
+
+    @Test
+    public void testNgInProcess() {
+        SurefireLauncher launcher = unpack("surefire-1364");
+        OutputValidator validator = launcher.setForkJvm()
+                .activateProfile("testng")
+                .forkNever()
+                .executeTest()
+                .verifyErrorFree(3);
+
+        validator
+                .getSurefireReportsXmlFile("TEST-FirstTest.xml")
+                .assertContainsText("<property name=\"forkedProp\" value=\"forkedValue1\"/>");
+    }
+}
---
+++ surefire-its/src/test/java/org/apache/maven/surefire/its/jiras/Surefire832ProviderSelectionIT.java
@@ -0,0 +1,48 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package org.apache.maven.surefire.its.jiras;
+
+import org.apache.maven.surefire.its.fixture.OutputValidator;
+import org.apache.maven.surefire.its.fixture.SurefireJUnit4IntegrationTestCase;
+import org.apache.maven.surefire.its.fixture.SurefireLauncher;
+import org.junit.Test;
+
+/**
+ *
+ */
+public class Surefire832ProviderSelectionIT extends SurefireJUnit4IntegrationTestCase {
+    @Test
+    public void testJUnitRunCategoryAB() {
+        OutputValidator validator =
+                unpackJUnit().groups("junit4.CategoryA,junit4.CategoryB").executeTest();
+        validator.verifyErrorFreeLog();
+        validator.assertTestSuiteResults(6, 0, 0, 0);
+        validator.verifyTextInLog("catA: 2");
+        validator.verifyTextInLog("catB: 2");
+        validator.verifyTextInLog("catC: 0");
+        validator.verifyTextInLog("catNone: 0");
+        validator.verifyTextInLog("mA: 2");
+        validator.verifyTextInLog("mB: 2");
+        validator.verifyTextInLog("mC: 0");
+    }
+
+    private SurefireLauncher unpackJUnit() {
+        return unpack("surefire-832-provider-selection");
+    }
+}
