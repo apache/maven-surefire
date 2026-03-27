7f40092b352a1fae2851a1f2d6a56df1d5125749
Christian Wegener <cwegener-79@christianwegener.de>
2026-03-27 15:29:24 +0100

    Fix import (JUnit 5)

--- surefire-its/src/test/java/org/apache/maven/surefire/its/AdditionalClasspathForkIT.java
+++ surefire-its/src/test/java/org/apache/maven/surefire/its/AdditionalClasspathForkIT.java
@@ -19,7 +19,7 @@
 package org.apache.maven.surefire.its;
 
 import org.apache.maven.surefire.its.fixture.SurefireJUnit4IntegrationTestCase;
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
 
 /**
  * Integration test for relative {@code additionalClasspathElement} entries resolved against
