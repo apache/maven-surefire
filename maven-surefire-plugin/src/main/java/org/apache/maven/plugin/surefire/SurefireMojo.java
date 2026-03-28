d7a6ac6bb9e20a3fec15f2299f2f5f1cec35a20b
Slawomir Jaranowski <s.jaranowski@gmail.com>
2026-03-28 14:26:43 +0100

    fix deprecation notices for suiteXmlFiles parameter
    - `@deprecated` annotation should be placed after docs ...
    - drop some of the unused methods

--- maven-surefire-plugin/src/main/java/org/apache/maven/plugin/surefire/SurefireMojo.java
+++ maven-surefire-plugin/src/main/java/org/apache/maven/plugin/surefire/SurefireMojo.java
@@ -287,7 +287,6 @@ public class SurefireMojo extends AbstractSurefireMojo implements SurefireReport
     private int failOnFlakeCount;
 
     /**
-     * @deprecated not supported after 3.6.0, please use groups or Junit suite support
      * (TestNG) List of &lt;suiteXmlFile&gt; elements specifying TestNG suite xml file locations. Note that
      * {@code suiteXmlFiles} is incompatible with several other parameters of this plugin, like
      * {@code includes} and {@code excludes}.<br>
@@ -295,6 +294,7 @@ public class SurefireMojo extends AbstractSurefireMojo implements SurefireReport
      * instead of an entire suite).
      *
      * @since 2.2
+     * @deprecated not supported after 3.6.0, please use groups or Junit suite support
      */
     @Deprecated
     @Parameter(property = "surefire.suiteXmlFiles")
