0e877c3495c14c96657444545cc0112428bfb09c
Vaclav Haisman <vhaisman@gmail.com>
2026-08-26 08:48:01 +0200

    Add failing IT for #3446
    The direct selection of a JUnit Jupiter @Nested class runs no tests on current master, although the same selection runs with Surefire 3.5.4.
    
    Refs #3446

---
+++ surefire-its/src/test/resources/surefire-3446-nested-selection/src/test/java/issue3446/NestedTest.java
@@ -0,0 +1,56 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package issue3446;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.fail;
+
+class NestedTest {
+    private boolean outerSetUp;
+
+    @BeforeEach
+    void setUpOuter() {
+        outerSetUp = true;
+    }
+
+    @Test
+    void outerTestMustNotRun() {
+        fail("The outer test must not run");
+    }
+
+    @Nested
+    class Selected {
+        @Test
+        void selectedTest() {
+            assertTrue(outerSetUp);
+        }
+    }
+
+    @Nested
+    class Sibling {
+        @Test
+        void siblingTestMustNotRun() {
+            fail("A sibling nested test must not run");
+        }
+    }
+}
